32206e8fee773245ec5288cab40c9664675b37f0
samalsubrat <samalsubrat919@gmail.com>
2025-08-06 07:56:46 +0530

    next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    JUDGE0_API_URL: process.env.JUDGE0_API_URL || "http://localhost:2358",
+    JUDGE0_SELF_URL: process.env.JUDGE0_SELF_URL || "http://localhost:2358",
   },
 };
 
